33af981f3891d8fa4e87b6c49028131f8c782387
Rajas Bansal <rajas@Rajas-MacBook-Pro.local>
2017-06-29 19:40:35 +0530

    better colour functionality

--- assets/controllers/login.js
+++ assets/controllers/login.js
@@ -5,6 +5,11 @@ App.LoginController = Ember.Controller.extend({
 	password: '',
 	username: '',
 	boolC: false,
+	colour_to_show:'',
+	text_colour: '',
+	colors : [   "#B8D430", "#3AB745", "#029990", "#3501CB",
+                   "#2E2C75", "#673A7E", "#CC0071", "#F80120",
+                   "#F35B20", "#FB9A00", "#FFCC00", "#FEF200"],
 	actions:{
 		// chooseColour: function(selection){
 		// 	this.set('colour', selection);
@@ -31,6 +36,45 @@ App.LoginController = Ember.Controller.extend({
 		set: function (col){
 			this.set('colour', col);
 			this.set('boolC', true);
+			var index;
+			if (col == 'Light Green'){
+				index=0;
+			}
+			else if (col == 'Dark Green'){
+				index=1;
+			}
+			else if (col == 'Teal'){
+				index=2;
+			}
+			else if (col == 'Blue'){
+				index=3;
+			}
+			else if (col == 'Dark Blue'){
+				index=4;
+			}
+			else if (col == 'Purple'){
+				index=5;
+			}
+			else if (col == 'Magenta'){
+				index=6;
+			}
+			else if (col == 'Red'){
+				index=7;
+			}
+			else if (col == 'Orange'){
+				index=8;
+			}
+			else if (col == 'Light Orange'){
+				index=9;
+			}
+			else if (col == 'Mango Yellow'){
+				index=10;
+			}
+			else if (col == 'Yellow'){
+				index=11;
+			}
+			this.set('colour_to_show', this.get('colors')[index])
+			this.set('text_colour', 'white');
 			console.log(this.get('colour'));
 		}
 	}
